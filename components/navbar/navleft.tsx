e64ed2fd1de2942ec84570429468d03128501299
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-27 14:51:43 +0200

    añado iconos a la nav

--- components/navbar/navleft.tsx
+++ components/navbar/navleft.tsx
@@ -1,45 +1,53 @@
-import React, { CSSProperties } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
+import React, { CSSProperties } from "react";
+import Cookies from "js-cookie";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navleft: React.FC = () => {
   const router = useRouter();
 
   const handleLogout = async () => {
-    Cookies.remove('authvalue');
+    Cookies.remove("authvalue");
     try {
-      const res = await fetch('http://localhost:5000/auth/logout', {
-        method: 'POST',
-        credentials: 'include',
+      const res = await fetch("http://localhost:5000/auth/logout", {
+        method: "POST",
+        credentials: "include",
       });
       if (res.status === 200) {
-        router.push('/login');
+        router.push("/login");
       } else {
-        console.error('No se pudo cerrar sesión');
+        console.error("No se pudo cerrar sesión");
       }
     } catch (error) {
-      console.error('Error al cerrar sesión', error);
+      console.error("Error al cerrar sesión", error);
     }
-    router.push('/');
+    router.push("/");
   };
 
   return (
-    <nav style={navStyles}>
-      {/* Primer div */}
-      <div style={columnStyles}>
-        <div>
+    <nav className="bg-slate-800 text-white py-5 pr-20 pl-10 w-200 fixed top-0 left-0 h-screen flex flex-col justify-between ">
+      <div className="flex flex-col text-left">
+        <div className="font-bold pt-5">
           <Link href="/">
             <p>EMPLEOTECNIC.COM</p>
           </Link>
         </div>
-        <div>
-          <h2>NombreUsuario</h2>
+
+        <div className="flex flex-row align-center py-2 ">
+          <h2 className="align-center py-2 pr-2">NombreUsuario</h2>
+          <div className="flex bg-white rounded-full overflow-hidden align-center text-center ">
+            <Image
+              src="/profileico.png"
+              alt="Profile Icon"
+              width={40}
+              height={40}
+              />
+          </div>
         </div>
       </div>
 
-      {/* Segundo div */}
-      <div style={columnStyles}>
+       <div className="flex flex-col">
         <div>
           <h2>Inicio</h2>
         </div>
@@ -54,31 +62,11 @@ const Navleft: React.FC = () => {
         </div>
       </div>
 
-      {/* Componente Cerrarsesion */}
-      <div style={columnStyles}>
+      <div className="flex flex-row">
         <button onClick={handleLogout}>Cerrar Sesión</button>
       </div>
     </nav>
   );
-};
-
-const navStyles: CSSProperties = {
-  backgroundColor: '#333',
-  color: '#fff',
-  padding: '20px',
-  width: '200px',
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-};
-
-const columnStyles: CSSProperties = {
-  display: 'flex',
-  flexDirection: 'column',
 };
 
 export default Navleft;
--- components/navbar/navtop.tsx
+++ components/navbar/navtop.tsx
@@ -1,36 +1,37 @@
-import React, { CSSProperties } from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
+import React, { CSSProperties } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 
 const Navtop: React.FC = () => {
   const router = useRouter();
 
   const handleLogout = async () => {
-    Cookies.remove('authvalue');
+    Cookies.remove("authvalue");
     try {
-      const res = await fetch('http://localhost:5000/auth/logout', {
-        method: 'POST',
-        credentials: 'include',
+      const res = await fetch("http://localhost:5000/auth/logout", {
+        method: "POST",
+        credentials: "include",
       });
       if (res.status === 200) {
-        router.push('/login');
+        router.push("/login");
       } else {
-        console.error('No se pudo cerrar sesión');
+        console.error("No se pudo cerrar sesión");
       }
     } catch (error) {
-      console.error('Error al cerrar sesión', error);
+      console.error("Error al cerrar sesión", error);
     }
-    router.push('/');
+    router.push("/");
   };
 
   return (
-    <nav  className='flex flex-row bg-white '>
+    <nav className="flex flex-row bg-white ">
       <div className="pt-7 pb-3 px-20 bg-white  shadow">
-              <h1 className="text-cyan-950 px-10 text-xl ">
-                El contenido de esta nav debe ser dinámico y afectar al resto del contenido
-              </h1>
-            </div>
+        <h1 className="text-cyan-950 px-10 text-xl ">
+          El contenido de esta nav debe ser dinámico y afectar al resto del
+          contenido
+        </h1>
+      </div>
     </nav>
   );
 };
